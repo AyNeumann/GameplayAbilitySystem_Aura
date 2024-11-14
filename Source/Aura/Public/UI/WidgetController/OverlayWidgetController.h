6956ddb40e66ac8ed7ec983abcdc5f55051d1f16
Aymeric Neumann <ay.neumann@laposte.net>
2024-11-14 11:31:58 +0100

    Overlay Widget Controller

---
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -0,0 +1,17 @@
+// Aymeric Neumann - "Create a multiplayer RPG with Unreal Engine's Gameplay Ability System (GAS)!" tutorial on Udemy by Stephen Ulibarri
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+#include "OverlayWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UOverlayWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+	
+};
