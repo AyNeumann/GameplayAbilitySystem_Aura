263907a4b14ba4d745cfa954380e881389e955c8
Aymeric Neumann <ay.neumann@laposte.net>
2024-11-14 09:44:53 +0100

    Fix Copyright notice

--- Source/Aura/Private/Interaction/EnemyInterface.cpp
+++ Source/Aura/Private/Interaction/EnemyInterface.cpp
@@ -1,4 +1,4 @@
-// // Aymeric Neumann - "Create a multiplayer RPG with Unreal Engine's Gameplay Ability System (GAS)!" tutorial on Udemy by Stephen Ulibarri
+// Aymeric Neumann - "Create a multiplayer RPG with Unreal Engine's Gameplay Ability System (GAS)!" tutorial on Udemy by Stephen Ulibarri
 
 
 #include "Interaction/EnemyInterface.h"
